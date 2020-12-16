7a244a7a91500f4eed4dbebcf3b3dd857c3977de
Cameron <cstanl16@uncc.edu>
2020-12-15 19:31:12 -0500

    updated a few things

--- src/auth0-provider-with-history.js
+++ src/auth0-provider-with-history.js
@@ -4,12 +4,14 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const Auth0ProviderWithHistory = ({ children }) => {
     const history = useHistory();
-    //const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-    //const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-    const domain = "dev-3ylt35nh.us.auth0.com";
-    const clientId = "PkJFJGyBpPfPSSrv1MvrSBS9ygoi8kzP";
-    console.log(domain);
-    console.log(clientId);
+    const domainVar = process.env.REACT_APP_AUTH0_DOMAIN;
+    const clientIdVar = process.env.REACT_APP_AUTH0_CLIENT_ID;
+    const domain = 'dev-3ylt35nh.us.auth0.com';
+    const clientId = 'PkJFJGyBpPfPSSrv1MvrSBS9ygoi8kzP';
+    console.log("this is my domain: " + domain);
+    console.log("this is my clientId: " + clientId);
+    console.log("this is my domainVar: " + domainVar);
+    console.log("this is my clientIdVar: " + clientIdVar);
 
     const onRedirectCallback = (appState) => { 
         history.push(appState?.returnTo || window.location.pathname);
