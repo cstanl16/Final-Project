5ef638fd3868827ae7602579779f843de9327fd3
Cameron Stanley <cstanl16@uncc.edu>
2022-11-10 21:23:25 -0500

    auth0 info update

--- src/auth0-provider-with-history.js
+++ src/auth0-provider-with-history.js
@@ -7,7 +7,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
     //const domainVar = process.env.REACT_APP_AUTH0_DOMAIN;
     //const clientIdVar = process.env.REACT_APP_AUTH0_CLIENT_ID;
     const domain = 'dev-3ylt35nh.us.auth0.com';
-    const clientId = 'PkJFJGyBpPfPSSrv1MvrSBS9ygoi8kzP'; //SSH into server to add this into the .env file since its hidden in git
+    const clientId = 'vaGxJCFETocOmTZfw2XxFIKNsoPg4YQY'; //SSH into server to add this into the .env file since its hidden in git
 
     const onRedirectCallback = (appState) => { 
         history.push(appState?.returnTo || window.location.pathname);
